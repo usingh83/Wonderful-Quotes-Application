a0d68fadd89d604db6a61532c6db3c9a0d551b6a
Uday Singh <kanwarudaysingh@gmail.com>
2018-08-23 09:47:01 -0500

    Final Commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="container">
+        <app-header :quoteCount="quotes.length" :maxCount="maxquotes"></app-header>
+        <app-new-quote @quoteAdded="newQuote"></app-new-quote>
+        <app-quotes-grid :quotes="quotes" @qouteDeleted="deleteQuote"></app-quotes-grid>
+        <div class="row">
+            <div class="col-sm-12 text-center">
+                <div class="alert alert-info">
+                    Info: Click on Quote to delete it!
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import QuotesGrid from "./components/QuotesGrid.vue"
+import NewQuote from "./components/NewQuote.vue"
+import Header from "./components/Header.vue"
+    export default {
+        data:function(){
+            return {
+                quotes:['What a wonderful world'],
+                maxquotes:10
+            }
+        },
+        components:{
+            appQuotesGrid:QuotesGrid,
+            appNewQuote:NewQuote,
+            appHeader:Header
+        },
+        methods:{
+            newQuote:function(quote){
+                this.quotes.push(quote)
+            },
+            deleteQuote:function(index){
+                this.quotes.splice(index,1)
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
---
+++ src/components/QuotesGrid.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+    <app-quotes v-for="(quote,index) in quotes" @click.native="deleteQuote(index)">
+        {{quote}}
+    </app-quotes>
+</div>
+</template>
+<script>
+import Quotes from "./Quotes.vue"
+    export default{
+        props:['quotes'],
+        components:{
+            appQuotes:Quotes
+        },
+        methods:{
+            deleteQuote:function(index){
+                this.$emit('qouteDeleted',index)
+            }
+        }
+    }
+</script>
